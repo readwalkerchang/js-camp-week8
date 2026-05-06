66c53545990cd13c30d2697c7d4582da8b5103a3
Dusing Chang <dusing.chang@mendix.com>
2026-05-06 21:17:30 +0200

    Implement product fetching logic in productService functions

--- services/productService.js
+++ services/productService.js
@@ -13,6 +13,8 @@ async function getProducts() {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得產品陣列
   // 回傳格式：{ products, count: 產品數量 }
+  const products = await fetchProducts();
+  return { products, count: products.length };
 }
 
 /**
@@ -24,6 +26,8 @@ async function getProductsByCategory(category) {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得所有產品後，篩選出符合 category 的產品
   // 回傳格式：篩選後的產品陣列
+  const products = await fetchProducts();
+  return products.filter(item => item.category === category);
 }
 
 /**
@@ -35,6 +39,12 @@ async function getProductById(productId) {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得所有產品後，找出 id 符合的產品
   // 若找不到，回傳 null
+  const products = await fetchProducts();
+  const matchProducts =  products.find(item => item.id === productId);
+  if(matchProducts === undefined){
+    return null;
+  }
+  return matchProducts;
 }
 
 /**
@@ -44,6 +54,8 @@ async function getProductById(productId) {
 async function getCategories() {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得所有產品後，代入到 utils getAllCategories()
+  const products = await fetchProducts();
+  return getAllCategories(products)
 }
 
 /**
