6269ddb959e48e746462d627d77deba1d845f308
Dusing Chang <dusing.chang@mendix.com>
2026-05-07 10:46:20 +0200

    Add color configuration to Jest output and enhance output formatting

--- scripts/test-output.js
+++ scripts/test-output.js
@@ -3,12 +3,18 @@ const fs = require('fs');
 
 const jestBin = require.resolve('jest/bin/jest');
 
-const result = spawnSync(process.execPath, [jestBin, '--runInBand'], {
+const result = spawnSync(process.execPath, [jestBin, '--runInBand', '--no-color'], {
   encoding: 'utf8',
-  stdio: 'pipe'
+  stdio: 'pipe',
+  env: {
+    ...process.env,
+    FORCE_COLOR: '0',
+    NO_COLOR: '1'
+  }
 });
 
-const output = `${result.stdout || ''}${result.stderr || ''}`;
+const output = `${result.stdout || ''}${result.stderr || ''}`
+  .replace(/\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])/g, '');
 
 fs.writeFileSync('test output.md', output);
 
